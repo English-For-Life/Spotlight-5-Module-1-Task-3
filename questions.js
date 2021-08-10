ebf11617c3bdd4549613473a6aaf4990f3cfd5a2
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-10 13:41:59 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"... pencil case",
+        options:["a", "an"],
+        answer: 0
+    },
+    {
+        q:"... atlas",
+        options:["a", "an"],
+        answer: 1
+    },
+    {
+        q:"... pencil sharpener",
+        options:["a", "an"],
+        answer: 0
+    },
+    {
+        q:"... eraser",
+        options:["a", "an"],
+        answer: 1
+    },
+    {
+        q:"... ruler",
+        options:["a", "an"],
+        answer: 0
+    }
+
+
+
+]
